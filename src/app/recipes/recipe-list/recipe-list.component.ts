baafb7e77637e4e16197cd384dde6391f34d9a06
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2023-01-03 16:57:18 +0100

    adding ingrediant and display

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.tastingtable.com/img/gallery/15-recipes-to-upgrade-spaghetti-night/intro-1672261489.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.tastingtable.com/img/gallery/15-recipes-to-upgrade-spaghetti-night/intro-1672261489.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.tastingtable.com/img/gallery/15-recipes-to-upgrade-spaghetti-night/intro-1672261489.jpg'
+    ),
+  ];
 
   constructor() {
   }
